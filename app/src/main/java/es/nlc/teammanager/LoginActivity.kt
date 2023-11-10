a1d21c6cc681f0a2dcb743810db28de091a6d8b3
Natall1122 <97448973+Natall1122@users.noreply.github.com>
2023-11-10 18:55:35 +0100

    Initial commit

---
+++ app/src/main/java/es/nlc/teammanager/LoginActivity.kt
@@ -0,0 +1,38 @@
+package es.nlc.teammanager
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import es.nlc.teammanager.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+
+    private var registrat = false
+    private lateinit var binding: ActivityLoginBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDialogClick() {
+        Snackbar.make(binding.root, getString(R.string.creat), Snackbar.LENGTH_SHORT).show()
+        registrat = true
+    }
+
+    override fun onButtonClicked() {
+        if (registrat == true){
+            val main = Intent(this, MainActivity::class.java)
+            startActivity(main)
+        }else{
+            Snackbar.make(binding.root, getString(R.string.noreg), Snackbar.LENGTH_SHORT).show()
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/es/nlc/teammanager/LoginFragment.kt
@@ -0,0 +1,51 @@
+package es.nlc.teammanager
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import es.nlc.teammanager.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment(), View.OnClickListener {
+    private lateinit var binding: FragmentLoginBinding
+    private var mListener: OnButtonsFragmentListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if(context is OnButtonsFragmentListener){
+            mListener = context
+        }else{
+            throw Exception("The activity must implement the interface OnButtonsFragmentListener")
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.btnLog.setOnClickListener(this)
+        binding.btnReg.setOnClickListener {
+            DialogFragment().show(requireActivity().supportFragmentManager,"")
+        }
+        return binding.root
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_log -> mListener?.onButtonClicked()
+        }
+    }
+    override fun onDetach() {
+        super.onDetach()
+        mListener = null
+    }
+
+    interface OnButtonsFragmentListener{
+        fun onButtonClicked()
+    }
+
+
+}
