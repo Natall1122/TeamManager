b91337fe309829597c4a4b6ef618e1af2efa9e54
Natall1122 <97448973+Natall1122@users.noreply.github.com>
2024-02-12 20:26:02 +0100

    changing strings

--- app/src/main/java/es/nlc/teammanager/fragments/RetrofitFragment.kt
+++ app/src/main/java/es/nlc/teammanager/fragments/RetrofitFragment.kt
@@ -6,10 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import es.nlc.teammanager.CatsInterface
 import es.nlc.teammanager.adapters.CatsAdapter
-import es.nlc.teammanager.clases.Cats
 import es.nlc.teammanager.databinding.FragmentRetrofitBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,7 +18,7 @@ import kotlinx.coroutines.withContext
 class RetrofitFragment : Fragment() {
     private lateinit var binding: FragmentRetrofitBinding
     val cats = mutableListOf<String>()
-    val productsAdapter = CatsAdapter( context, cats)
+    private lateinit var productsAdapter: CatsAdapter
     val llistaTemporal = mutableListOf<String>()
 
 
@@ -27,31 +27,34 @@ class RetrofitFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRetrofitBinding.inflate(inflater, container, false)
+        setReciclerView()
+
 
         binding.buscar.setOnClickListener {
             lifecycleScope.launch(Dispatchers.IO){
                 val call = RetrofitObject.getInstance()
                     .create(CatsInterface::class.java).getCats()
                 val response = call.body()
+                llistaTemporal.clear()
+
                 response?.forEach { element ->
                     llistaTemporal.add(element.url)
                 }
 
                 withContext(Dispatchers.Main){
-                    withContext(Dispatchers.Main){
-                        updateCats(llistaTemporal)
-                    }
+                    updateCats(llistaTemporal)
                 }
             }
-            setReciclerView()
+
         }
 
         return binding.root
     }
 
     fun setReciclerView(){
+        productsAdapter = CatsAdapter(requireActivity(), cats)
         binding.recView.adapter = productsAdapter
-        binding.recView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.recView.layoutManager = GridLayoutManager(context, 2)
     }
 
     private fun updateCats(gats: List<String>){
